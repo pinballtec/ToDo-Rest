769af50d4d331c55d42c2b86fa67b2b316e8605e
pinballtec <daniilboiko99@gmail.com>
2022-02-01 22:01:17 +0100

    settup of list of tasks

--- todo_app/views.py
+++ todo_app/views.py
@@ -4,6 +4,8 @@ from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
 
 
 @api_view(['GET'])
@@ -16,3 +18,10 @@ def apiOverview(request):
         'Delete': '/task-delete/<str:pk>',
     }
     return Response(api_urls)
+
+
+@api_view(['GET'])
+def task_list(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
