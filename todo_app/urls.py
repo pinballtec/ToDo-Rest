fc52f6be34c1e0bc3b7f0f8eaca50aed46cede65
pinballtec <daniilboiko99@gmail.com>
2022-02-02 17:17:07 +0100

    all ready to launch)

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.apiOverview, name='api-overview'),
-    path('task-list/', views.task_list, name='tasks')
+    path('task-list/', views.task_list, name='tasks'),
+    path('task-detail/<str:pk>/', views.task_detail, name='tasks-detail'),
+    path('task-create/', views.task_create, name='tasks-create'),
+    path('task-update/<str:pk>/', views.task_update, name='tasks-update')
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -25,3 +25,34 @@ def task_list(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def task_detail(request, pk):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(tasks, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def task_create(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def task_update(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def task_delete(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+    return Response('DELETED')
